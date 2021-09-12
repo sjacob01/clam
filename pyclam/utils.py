looking for normalization bug

pyclam/utils.py
@@ -1,5 +1,8 @@
 """ Some common functions and constants for all of CLAM.
 """
+
+import numpy
+
 SUBSAMPLE_LIMIT = 100
 BATCH_SIZE = 10_000
 EPSILON = 1e-8
@@ -23,23 +26,21 @@ def normalize(values, mode: str):
     :param mode: Normalization mode to use. Must be one of 'linear', 'gaussian', or 'sigmoid'.
     :return: array of normalized values.
     """
-    import numpy as np
-
     squeeze = False
     if len(values.shape) == 1:
         squeeze = True
-        values = np.expand_dims(values, axis=1)
+        values = numpy.expand_dims(values, axis=1)
 
     if mode == 'linear':
-        min_v, max_v = np.min(values, axis=0), np.max(values, axis=0)
+        min_v, max_v = numpy.min(values, axis=0), numpy.max(values, axis=0)
         for i in range(values.shape[1]):
             if min_v[i] == max_v[i]:
                 max_v[i] += 1
                 values[:, i] = min_v[i] + 0.5
         values = (values - min_v) / (max_v - min_v)
     else:
-        mu = np.mean(values, axis=0)
-        sigma = np.std(values, axis=0)
+        mu = numpy.mean(values, axis=0)
+        sigma = numpy.std(values, axis=0)
 
         for i in range(values.shape[1]):
             if sigma[i] < EPSILON:
@@ -48,11 +49,11 @@ def normalize(values, mode: str):
                 if mode == 'gaussian':
                     from scipy.special import erf
 
-                    values[:, i] = (1 + erf((values[:, i] - mu[i]) / (sigma[i] * np.sqrt(2)))) / 2
+                    values[:, i] = (1 + erf((values[:, i] - mu[i]) / (sigma[i] * numpy.sqrt(2)))) / 2
                 else:
-                    values[:, i] = 1 / (1 + np.exp(-(values[:, i] - mu[i]) / sigma[i]))
+                    values[:, i] = 1 / (1 + numpy.exp(-(values[:, i] - mu[i]) / sigma[i]))
 
     values = values.clip(EPSILON, 1)
     if squeeze:
-        values = np.squeeze(values)
+        values = numpy.squeeze(values)
     return values